08d9ce2fd41fa93a39a4a9178b2634f32224b472
ABHIJITHAJ7 <ajstorm2008@gmail.com>
2025-05-16 16:03:56 +0530

    Update Contact.js

--- models/Contact.js
+++ models/Contact.js
@@ -1,10 +1,12 @@
-
+// models/Contact.js
+const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
   name: String,
   email: String,
   message: String,
-  date: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Contact', contactSchema);
+
